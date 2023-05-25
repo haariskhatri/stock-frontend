b035826eb43e92182a87cc5af57c4daa650da344
haariskhatri <haarishkkhatri@gmail.com>
2023-05-25 20:17:53 +0530

    added react router dom

--- stock-frontend/src/main.jsx
+++ stock-frontend/src/main.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import IpoRegister from "./pages/IpoRegister";
+import './App.css'
+import BuySellCard from "./components/BuySellCard";
+import MarketPage from "./pages/MarketPage";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <IpoRegister />,
+  },
+  {
+    path: '/buy',
+    element: <MarketPage />
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
