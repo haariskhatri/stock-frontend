e004a97ec0b060218258c7b8269a3de00fc29725
haariskhatri <haarishkkhatri@gmail.com>
2023-05-30 19:33:28 +0530

    created sidemenu for admin

--- stock-frontend/src/components/sidemenu.jsx
+++ stock-frontend/src/components/sidemenu.jsx
@@ -1,8 +1,22 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const SideMenu = () => {
     return (
         <>
+            <div className="side-menu">
+
+
+                <ul className="list-unstyled">
+                    <li><NavLink to='/' > <i class="fa-solid fa-house"></i> Home</NavLink></li>
+                    <li><NavLink to='/dasd' ><i class="fa-solid fa-square-plus"></i>Add IPO</NavLink></li>
+                    <li><NavLink to='/sda' ><i class="fa-solid fa-circle-info"></i>Details</NavLink></li>
+                    {/* <li><NavLink to='/dsa' >Item</NavLink></li> */}
+                    <li><NavLink to='/dss' ><i class="fa-solid fa-right-from-bracket"></i>Log Out</NavLink></li>
+                </ul>
+
+
+            </div >
         </>
     )
 }
