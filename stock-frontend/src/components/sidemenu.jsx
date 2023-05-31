af5a95a071c9a0bb357c8e323423775307dccbb2
haariskhatri <haarishkkhatri@gmail.com>
2023-05-31 19:53:45 +0530

    added working links

--- stock-frontend/src/components/sidemenu.jsx
+++ stock-frontend/src/components/sidemenu.jsx
@@ -8,8 +8,8 @@ const SideMenu = () => {
 
 
                 <ul className="list-unstyled">
-                    <li><NavLink to='/' > <i class="fa-solid fa-house"></i> Home</NavLink></li>
-                    <li><NavLink to='/dasd' ><i class="fa-solid fa-square-plus"></i>Add IPO</NavLink></li>
+                    <li><NavLink to='/home' > <i class="fa-solid fa-house"></i> Home</NavLink></li>
+                    <li><NavLink to='/register' ><i class="fa-solid fa-square-plus"></i>Add IPO</NavLink></li>
                     <li><NavLink to='/sda' ><i class="fa-solid fa-circle-info"></i>Details</NavLink></li>
                     {/* <li><NavLink to='/dsa' >Item</NavLink></li> */}
                     <li><NavLink to='/dss' ><i class="fa-solid fa-right-from-bracket"></i>Log Out</NavLink></li>
