fdb82960967ceab41b781f4579a5ddd20ae94459
davekishan <75985572+davekishan@users.noreply.github.com>
2023-06-14 12:03:05 +0530

    remove extra

--- stock-frontend/src/components/IpoAdmin.jsx
+++ stock-frontend/src/components/IpoAdmin.jsx
@@ -5,6 +5,7 @@ import { PreLoader } from './PreLoader';
 export const IpoAdmin = () => {
     const [data, setData] = useState([]);
     const [loader,setloader]=useState(true)
+    const [ipodetail,setipodetail]=useState([]);
     
     var componyId ;
     const navigate = useNavigate()
@@ -61,19 +62,20 @@ export const IpoAdmin = () => {
         <div className='container'>
             <center><h3 className='card-title'>Compony Ipo</h3></center>
 
-            {console.log(data)}
+            {console.log(data.ipodata)}
+            
             {data?.ipo?.map((ipo, key) => {
                 
                 return(
                     <div className="card ipoadmin">
                         <h5 className="card-title">{ipo.companyName}</h5>
                         <div className="card-body">
-                            <h5 className="card-title">Raise Money 10000000 Rs</h5>
-                            <p className="card-text">Total Subscriber 90/100</p>
+                            <h5 className="card-title">Allocate Slot</h5>
+                           
                             <div className='buy-button'>
 
-                                <button className="btn btn-primary" onClick={()=>{componyId=ipo.companyId; allocate_slot()}}>Allocate Slot</button>
-                                <button className="btn btn-primary" onClick={()=>{componyId=ipo.companyId; cancel_ipo()}}>Cancel IPO</button>
+                                <button className="btn btn-primary" onClick={()=>{componyId=ipo.companyId; allocate_slot(ipo.companyId)}}>Allocate Slot</button>
+                                <button className="btn btn-primary" onClick={()=>{componyId=ipo.companyId; cancel_ipo(ipo.companyId)}}>Cancel IPO</button>
                             </div>
 
                         </div>
